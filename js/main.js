f33fe82869a97c5c3207a01957c3d6935d041ab8
fayzullayevelmurod <elmurodfayzullayev77@gmail.com>
2024-06-15 23:34:02 +0500

    investments desktop in progress

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// STRAIGHT MARQUEE
+// MARQUEE
 let marqueeCarousel = new Swiper(".marquee-carousel", {
     autoplay: {
         delay: 1,
@@ -7,25 +7,9 @@ let marqueeCarousel = new Swiper(".marquee-carousel", {
     },
     slidesPerView: 10,
     speed: 6000, // for marquee speed
-    spaceBetween: 50,
     resistance: true,
     resistanceRatio: 0,
     loop: true,
     allowTouchMove: false,
 });
-  // REVERSE MARQUEE
-let reverseMarqueeCarousel = new Swiper(".reverse-marquee-carousel", {
-    autoplay: {
-        delay: 1,
-        disableOnInteraction: false,
-        pauseOnMouseEnter: false,
-        reverseDirection: true, // FOR REVERSE
-    },
-    slidesPerView: 1,
-    speed: 6000, // for marquee speed
-    spaceBetween: 50,
-    resistance: true,
-    resistanceRatio: 0,
-    loop: true,
-    allowTouchMove: false,
-});
+// MARQUEE
